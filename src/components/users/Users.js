434eb26029ac280789e4a24c4ac97b4c8b63cb6c
gisantiago <gisantiago@live.com>
2019-06-13 15:45:40 -0400

    *

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
 
 const Users = ({ users, loading }) => {
   if (loading) {
